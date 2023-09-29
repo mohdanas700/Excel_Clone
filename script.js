b82b99536b823d643d14e2893bf81ce31ccd786a
mohdanas700 <mohdanas.dev70@gmail.com>
2023-09-29 17:42:18 +0530

    Problem Resolved

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     // this block of code executes,after borwser loads.
 
-    let cellContainer = $(".input-cell-container")
+    // Select the cell container for easier manipulation.
+    let cellContainer = $(".input-cell-container");
 
+    // Loop to generate column names (A, B, C, ..., Z, AA, AB, ...).
     for (let i =1;i<=100;i++) {
         let ans = "" ;
 
@@ -20,10 +22,11 @@ $(document).ready(function(){
             }
         }
 
+        // Create and append column names to the column-name-container.
         let column = $(`<div class="column-name colId-${i}" id="colCod-${ans}">${ans}</div>`);
         $(".column-name-container").append(column);
-        // let row = $('<div class="row-name" id="rowId-${i}">${i}</div>')
-        // $(".row-name-container").append(row)
+        
+        // Create and append row names to the row-name-container.
         let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
         $(".row-name-container").append(row);
     }
@@ -32,6 +35,7 @@ $(document).ready(function(){
     // So during 1st loop we are creating row.
     // In second loop we are creating cell.
     // After creating cell appending it to row,then appending it to input-cell-container. 
+    // Create cells in a grid (100x100) and append them to the cell container.
     for (let i = 1;i <= 100; i++){
         let row = $('<div class="cell-row"></div>')
         for (let j = 1; j <= 100; j++){
@@ -42,21 +46,32 @@ $(document).ready(function(){
         $('.input-cell-container').append(row);
     }
 
-    // This function is used to add or remove the bgcolor when user click on align-items
+    // This function is used to add or remove the bgcolor when user click on align-items,
+    // This function handles the alignment icon click event.
     $('.align-icon').click(function() {
+        // Remove the 'selected' class from any previously selected alignment icons.
         $('.align-icon.selected').removeClass('selected');
+
+        // Add the 'selected' class to the clicked alignment icon to indicate the new selection.
         $(this).addClass('selected')
     });
 
     //This function is used to  add or remove the bgcolor when user click on font-format
     $('.style-icon').click(function() {
-        $(this).toggleClass('selected');
+
+        // This function toggles the 'selected' class when a style icon is clicked,
+        // allowing the user to easily toggle the style on or off.
+        $(this).toggleClass("selected");
     });
 
-//This function is used to  select the cell
+    //This function is used to  select the cell
+    // This function handles cell selection based on user interactions.
     $(".input-cell").click(function (e) {
+        // This function handles cell selection based on user interactions.
         if(e.ctrlKey) {
             let [rowId, colId] = getRowCol(this);
+
+            // Check for cell selection in the row above.
             if(rowId > 1) { 
                 let topCellSelected = $(`#row-${rowId-1}-col-${colId}`).hasClass("selected");
                 if(topCellSelected) {
@@ -64,6 +79,8 @@ $(document).ready(function(){
                     $(`#row-${rowId-1}-col-${colId}`).addClass("bottom-cell-selected");
                 }
             }
+
+            // Check for cell selection in the row below.
             if(rowId < 100) {
                 let bottomCellSelected = $(`#row-${rowId+1}-col-${colId}`).hasClass("selected");
                 if(bottomCellSelected) {
@@ -71,6 +88,8 @@ $(document).ready(function(){
                     $(`#row-${rowId+1}-col-${colId}`).addClass("top-cell-selected");
                 }
             }
+
+            // Check for cell selection in the column to the left.
             if(colId > 1) {
                 let leftCellSelected = $(`#row-${rowId}-col-${colId-1}`).hasClass("selected");
                 if(leftCellSelected) {
@@ -78,6 +97,8 @@ $(document).ready(function(){
                     $(`#row-${rowId}-col-${colId-1}`).addClass("right-cell-selected");
                 }
             }
+
+            // Check for cell selection in the column to the right.
             if(colId < 100) {
                 let rightCellSelected = $(`#row-${rowId}-col-${colId+1}`).hasClass("selected");
                 if(rightCellSelected) {
@@ -85,34 +106,89 @@ $(document).ready(function(){
                     $(`#row-${rowId}-col-${colId+1}`).addClass("left-cell-selected");
                 }
             }
+            // Add the 'selected' class to the current cell.
             $(this).addClass("selected");
         }
         else {
+            // If Ctrl key is not pressed, remove 'selected' class from all other selected cells.
             $(".input-cell.selected").removeClass("selected");
+
+            // Add the 'selected' class to the current cell.
             $(this).addClass("selected");
         }
     });
 
     $(".input-cell").dblclick(function () {
+        // This event handler is triggered when an input cell is double-clicked.
+
+        // Remove the 'selected' class from any previously selected cell.
         $(".input-cell.selected").removeClass("selected");
+
+        // Add the 'selected' class to the current input cell, making it the new selection.
         $(this).addClass("selected");
-        $(this).attr("contenteditable", "false");
+
+        // Enable content editing for the selected input cell.
+        $(this).attr("contenteditable", "true");
+
+        // Set focus to the selected input cell, allowing immediate editing.
         $(this).focus();
     });
 
+    
+    $(".input-cell").blur(function(){
+        // This event handler is triggered when an input cell loses focus.
+
+        // Disable content editing for the currently selected input cell.
+        $(".input-cell.selected").attr("contenteditable","false");
+
+        // Update the cell content with the text entered by the user.
+        updateCell("text", $(this).text());
+    })
+
     $(".input-cell-container").scroll(function () {
+        // This event handler is triggered when the input cell container is scrolled.
+        
+        // Synchronize the horizontal scroll position of the column-name-container
+        // with the scroll position of the input-cell-container.
         $(".column-name-container").scrollLeft(this.scrollLeft);
+
+        // Synchronize the vertical scroll position of the row-name-container
+        // with the scroll position of the input-cell-container.
         $(".row-name-container").scrollTop(this.scrollTop);
     })
 
 
 });
 
-function getRowCol(elem) {
-    // this function will return the selected row id and column id by the help of "elem"
-    let idArray = $(elem).attr("id").split("-");
+function getRowCol(ele) {
+    // This function extracts the selected row id and column id based on the provided element.
+    let idArray = $(ele).attr("id").split("-");
     let rowId = parseInt(idArray[1]);
     let colId = parseInt(idArray[3]);
-    return (rowId, colId)
+    return [rowId, colId]
 };
 
+function updateCell(property,value) {
+    // This function updates the specified CSS property with the given value for all selected cells.
+    $(".input-cell.selected").each(function(){
+        $(this).css(property,value);
+    });
+};
+
+$(".icon-bold").click(function() {
+   // This event handler toggles the "font-weight" property for selected cells when the bold icon is clicked. 
+    if($(this).hasClass("selected")) {
+        updateCell("font-weight","bold",true);
+    }
+    else {
+        updateCell("font-weight","",false);
+    }
+});
+
+// $(".icon-bold").click(function() {
+//     if($(this).hasClass("selected")) {
+//         updateCell("font-weight","",true);
+//     } else {
+//         updateCell("font-weight","bold",false);
+//     }
+// });
